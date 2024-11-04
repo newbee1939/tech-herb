762f3d985e4a998713fbb8562198c65b123671f4
newbee1939 <hideto3970@gmail.com>
2024-11-04 13:08:27 +0900

    コンポーネントの行数が多くなって管理しづらくなるので、データ取得処理は別ファイルに切り出した

---
+++ src/components/TechArticle/feeds/publickey.ts
@@ -0,0 +1,36 @@
+import { articleLimit } from "./constants/articleLimit";
+
+type PublicKeyArticle = {
+	title: string,
+	pubDate: Date,
+	link: string,
+	guid: string,
+	author: string,
+	thumbnail: string,
+	description: string,
+	content: string,
+	enclosure: {},
+	categories: string[],
+};
+
+export const getPublickey = async () => {
+    let slicedPublickeyArticles: TechArticle[] = [];
+    try {
+        const latestPublickeyArticles = (await(await fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://www.publickey1.jp/atom.xml&api_key=${import.meta.env.RSS_2_JSON_API_KEY}`)).json()).items;
+        slicedPublickeyArticles = latestPublickeyArticles.slice(0, articleLimit).map((article: PublicKeyArticle) => {
+            return {
+                title: article.title,
+                link: article.link,
+            }
+        });
+    } catch(e) {
+        console.error(e);
+        slicedPublickeyArticles  = [];
+    }
+    console.log(slicedPublickeyArticles);
+    return {
+        name: "Publickey",
+        link: "https://www.publickey1.jp/",
+        items: slicedPublickeyArticles,
+    };
+}
