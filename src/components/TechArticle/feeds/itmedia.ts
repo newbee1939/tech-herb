4ffeef5b94440346019644f025f15dda7279f4c4
newbee1939 <hideto3970@gmail.com>
2024-11-05 08:59:12 +0900

    コンポーネントの行数が多くなって管理しづらくなるので、データ取得処理は別ファイルに切り出した

---
+++ src/components/TechArticle/feeds/itmedia.ts
@@ -0,0 +1,36 @@
+import { articleLimit } from "./constants/articleLimit";
+import { type TechArticle } from '../types/techArticle';
+
+type ItMediaArticle = {
+	title: string,
+	pubDate: Date,
+	link: string,
+	guid: string,
+	author: string,
+	thumbnail: string,
+	description: string,
+	content: string,
+	enclosure: {},
+	categories: string[],
+};
+
+export const getItMediaMedium = async () => {
+    let slicedItMediaArticles: TechArticle[] = [];
+    try {
+        const latestItMediaArticles = (await(await fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://rss.itmedia.co.jp/rss/2.0/topstory.xml&api_key=${import.meta.env.RSS_2_JSON_API_KEY}`)).json()).items;
+        slicedItMediaArticles = latestItMediaArticles.slice(0, articleLimit).map((article: ItMediaArticle) => {
+            return {
+                title: article.title,
+                link: article.link,
+            }
+        });
+    } catch(e) {
+        console.error(e);
+        slicedItMediaArticles = [];
+    }
+    return {
+        name: "ITmedia",
+        link: "https://www.itmedia.co.jp/",
+        items: slicedItMediaArticles,
+    }
+}
