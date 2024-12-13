b5f451876a5b1983c87e9d7e33298e1252bc6f95
newbee1939 <hideto3970@gmail.com>
2024-12-14 07:36:50 +0900

    Rakuten APIには1秒に1回未満の制限があるので、sleepの処理を追加

--- src/components/TechBook/apis/rakuten-book-api.ts
+++ src/components/TechBook/apis/rakuten-book-api.ts
@@ -6,7 +6,7 @@ const getLastMonth = (date = new Date()) => {
   const year = lastMonthDate.getFullYear();
   const month = lastMonthDate.getMonth() + 1; // NOTE:月は0から始まるので+1する
   return `${year}年${month}月`;
-};
+}
 
 const getToday = (date = new Date()) => {
   const year = date.getFullYear();
@@ -14,45 +14,50 @@ const getToday = (date = new Date()) => {
   const day = date.getDate();
 
   return `${year}年${month}月${day}日`;
-};
+}
+
+function sleep(second: number) {
+  return new Promise(resolve => setTimeout(resolve, second * 1000));
+}
 
 export const getTodayReleasedTechBooks = async (): Promise<TechBook[]> => {
-  let techBooks = [];
+  try {
+    let techBooks = [];
+    let count = 1;
 
-  // TODO: try-catch入れる
+    while (true) {
+      const techBooksPerPageResponse = await fetch(`https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?applicationId=${import.meta.env.RAKUTEN_APPLICATION_ID}&booksGenreId=001005&sort=%2DreleaseDate&elements=salesDate,title,itemUrl&page=${count}`);
+      const techBooksPerPage = (await techBooksPerPageResponse.json()).Items;
 
-  let count = 1;
-  while (true) {
-    const techBooksPerPageResponse = await fetch(`https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?applicationId=${import.meta.env.RAKUTEN_APPLICATION_ID}&booksGenreId=001005&sort=%2DreleaseDate&elements=salesDate,title,itemUrl&page=${count}`);
-    const techBooksPerPage = (await techBooksPerPageResponse.json()).Items;
+      techBooks.push(...techBooksPerPage);
 
-    console.log("=====================================");
-    console.log(count);
-    console.log(techBooksPerPage);
-    console.log("=====================================");
-    techBooks.push(...techBooksPerPage);
+      const lastMonth = getLastMonth();
+      const hasLastMonthPublishedBook = techBooksPerPage.some(book => book.Item.salesDate.includes(`${lastMonth}`));
 
-    const lastMonth = getLastMonth();
-    const hasLastMonthPublishedBook = techBooksPerPage.some(book => book.Item.salesDate.includes(`${lastMonth}`));
+      if (hasLastMonthPublishedBook) {
+        break;
+      }
 
-    if (hasLastMonthPublishedBook) {
-      break;
+      count++;
+
+      // NOTE: 利用制限(1秒に1回)に対処
+      // 参考: https://webservice.faq.rakuten.net/hc/ja/articles/900001974383-%E5%90%84API%E3%81%AE%E5%88%A9%E7%94%A8%E5%88%B6%E9%99%90%E3%82%92%E6%95%99%E3%81%88%E3%81%A6%E3%81%8F%E3%81%A0%E3%81%95%E3%81%84
+      await sleep(1.2);
     }
+    // 今日の発売の書籍に絞る
+    const todayPublishedTechBooks = techBooks.filter((book) => {
+      const today = getToday();
+      return book.Item.salesDate.includes(`${today}`);
+    }).map((book) => {
+      const { title, link} = book.Item;
+      return {
+        title,
+        link
+      }
+    });
 
-    count++;
+    return todayPublishedTechBooks;
+  } catch (e) {
+    console.error(e);
   }
-
-  // 今日の発売の書籍に絞る
-  const todayPublishedTechBooks = techBooks.filter((book) => {
-    const today = getToday();
-    return book.Item.salesDate.includes(`${today}`);
-  }).map((book) => {
-    const { title, link} = book.Item;
-    return {
-      title,
-      link
-    }
-  });
-
-  return todayPublishedTechBooks;
 }
