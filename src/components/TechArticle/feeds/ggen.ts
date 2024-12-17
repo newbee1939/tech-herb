a8f2b9bd0ef9abe241422835087ed66d9594920e
newbee1939 <hideto3970@gmail.com>
2024-12-17 23:40:16 +0900

    新着記事のみを表示する形に修正

--- src/components/TechArticle/feeds/ggen.ts
+++ src/components/TechArticle/feeds/ggen.ts
@@ -21,7 +21,17 @@ export const getGgenMedium = async () => {
     let slicedGgenArticles: TechArticle[] = [];
     try {
         const latestGgenArticles = (await(await fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://blog.g-gen.co.jp/feed&api_key=${import.meta.env.RSS_2_JSON_API_KEY}`)).json()).items;
-        slicedGgenArticles = latestGgenArticles.slice(0, articleLimit).map((article: GgenArticle) => {
+        slicedGgenArticles = latestGgenArticles.filter((article: GgenArticle) => {
+            const now = new Date();
+            // 前日の朝5時を計算
+            const yesterdayMorning5AM = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1, 5, 0, 0);
+
+            const articleDate = new Date(article.pubDate);
+
+            // NOTE: 前日の朝5時以降の情報を取得する
+            // そうすればmainマージ時に更新しても表示される内容は一定（前日の朝5時以降に投稿された記事）になる
+            return articleDate >= yesterdayMorning5AM;
+        }).slice(0, articleLimit).map((article: GgenArticle) => {
             return {
                 title: article.title,
                 link: article.link,
--- src/components/TechArticle/feeds/itmedia.ts
+++ src/components/TechArticle/feeds/itmedia.ts
@@ -18,7 +18,17 @@ export const getItMediaMedium = async () => {
     let slicedItMediaArticles: TechArticle[] = [];
     try {
         const latestItMediaArticles = (await(await fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://rss.itmedia.co.jp/rss/2.0/topstory.xml&api_key=${import.meta.env.RSS_2_JSON_API_KEY}`)).json()).items;
-        slicedItMediaArticles = latestItMediaArticles.slice(0, articleLimit).map((article: ItMediaArticle) => {
+        slicedItMediaArticles = latestItMediaArticles.filter((article: ItMediaArticle) => {
+            const now = new Date();
+            // 前日の朝5時を計算
+            const yesterdayMorning5AM = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1, 5, 0, 0);
+
+            const articleDate = new Date(article.pubDate);
+
+            // NOTE: 前日の朝5時以降の情報を取得する
+            // そうすればmainマージ時に更新しても表示される内容は一定（前日の朝5時以降に投稿された記事）になる
+            return articleDate >= yesterdayMorning5AM;
+        }).slice(0, articleLimit).map((article: ItMediaArticle) => {
             return {
                 title: article.title,
                 link: article.link,
