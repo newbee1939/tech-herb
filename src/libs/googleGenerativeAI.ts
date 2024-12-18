40cacd1872fce786b11cb291bd2d607b0765d407
newbee1939 <hideto3970@gmail.com>
2024-12-18 09:09:52 +0900

    様々なファイルから参照できるよう、Geminiを叩く処理を別ファイルに切り出した

---
+++ src/libs/googleGenerativeAI.ts
@@ -0,0 +1,13 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+export const answerFromGenerativeAi = async (prompt: string): Promise<string> => {
+    try {
+        const genAI = new GoogleGenerativeAI(import.meta.env.GEMINI_API_KEY);
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const result = await model.generateContent(prompt);
+        return result.response.text();
+    } catch (error) {
+        console.error('Error generating summary:', error);
+        throw error;
+    }
+};
