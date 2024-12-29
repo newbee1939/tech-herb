3d9d23d0092a4c0c29e2fd68fc0fecea845eb206
newbee1939 <hideto3970@gmail.com>
2024-12-29 18:07:52 +0900

    AIで要約されるようにした

--- src/components/TechArticle/feeds/publickey.ts
+++ src/components/TechArticle/feeds/publickey.ts
@@ -1,5 +1,7 @@
 import { articleLimit } from "../constants/articleLimit";
+import { answerFromGenerativeAi } from '../../../libs/googleGenerativeAI';
 import { type TechArticle } from '../types/techArticle';
+import { sleep } from '../../../utils/sleep';
 
 type PublicKeyArticle = {
 	title: string,
@@ -20,26 +22,36 @@ export const getPublickeyMedium = async () => {
         // TODO: リポジトリに切り出してもいいかも
         // ロジックのテストがしやすくなる
         const latestPublickeyArticles = (await(await fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://www.publickey1.jp/atom.xml&api_key=${import.meta.env.RSS_2_JSON_API_KEY}`)).json()).items;
-        slicedPublickeyArticles = latestPublickeyArticles.filter((article: PublicKeyArticle) => {
+        const slicedPublickeyArticlesPromises = latestPublickeyArticles.filter((article: PublicKeyArticle) => {
             // PR用の記事は除外
             return !article.title.includes("［PR］");
         }).filter((article: PublicKeyArticle) => {
             const now = new Date();
             // 前日の朝5時を計算
-            const yesterdayMorning5AM = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1, 5, 0, 0);
+            const yesterdayMorning5AM = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7, 5, 0, 0);
 
             const articleDate = new Date(article.pubDate);
 
             // NOTE: 前日の朝5時以降の情報を取得する
             // そうすればmainマージ時に更新しても表示される内容は一定（前日の朝5時以降に投稿された記事）になる
             return articleDate >= yesterdayMorning5AM;
-        }).slice(0, articleLimit).map((article: PublicKeyArticle) => {
+        }).slice(0, articleLimit).map(async (article: PublicKeyArticle) => {
+            const response = await fetch(article.link);
+            const bodyHtml = await response.text();
+
+            const prompt = `次のHTMLで書かれた技術記事を日本語で分かりやすく80文字程度で簡潔に要約してください！文体は「ですます調」でお願いします！！: ${bodyHtml}`;
+            const summarizedBody = await answerFromGenerativeAi(prompt);
+
+            // NOTE: Rate Limit対策
+            await sleep(6);
+
             return {
                 title: article.title,
                 link: article.link,
-                summarizedBody: "",
+                summarizedBody,
             }
         });
+        slicedPublickeyArticles = await Promise.all(slicedPublickeyArticlesPromises);
     } catch(e) {
         console.error(e);
         slicedPublickeyArticles  = [];
