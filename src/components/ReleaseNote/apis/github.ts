44dff7debd0ca2223a6a1722cac284191327126f
newbee1939 <hideto3970@gmail.com>
2025-01-15 22:33:36 +0900

    使っていない変数をコメントアウト

--- src/components/ReleaseNote/apis/github.ts
+++ src/components/ReleaseNote/apis/github.ts
@@ -44,7 +44,7 @@ type GitHubReleaseNote = {
 };
 
 const GITHUB_API_BASE = "https://api.github.com";
-const GITHUB_TOKEN = import.meta.env.GITHUB_TOKEN; // TODO: GitHubトークンを環境変数から取得
+// const GITHUB_TOKEN = import.meta.env.GITHUB_TOKEN; // TODO: GitHubトークンを環境変数から取得
 
 const libraries = [
   { name: "Astro", link: "https://github.com/withastro/astro/releases", owner: "withastro", repo: "astro" },
@@ -56,16 +56,15 @@ const libraries = [
  */
 async function getReleaseNotes(owner: string, repo: string) {
   const url = `${GITHUB_API_BASE}/repos/${owner}/${repo}/releases`;
-  const headers = GITHUB_TOKEN
-    ? { Authorization: `Bearer ${GITHUB_TOKEN}` }
-    : {};
+  // const headers = GITHUB_TOKEN
+  //   ? { Authorization: `Bearer ${GITHUB_TOKEN}` }
+  //   : {};
 
   try {
-    const releaseNotes = (await(await fetch(url, { headers })).json());
+    // const releaseNotes = (await(await fetch(url, { headers })).json());
+    const releaseNotes = (await(await fetch(url)).json());
     const mappedReleaseNotesPromises = releaseNotes.filter((releaseNote: GitHubReleaseNote) => {
       // TODO: グローバルなutilsに切り出す(Vitestでテストも書く)
-      // より分かりやすい実装にしたい
-      console.log(releaseNote);
       const today = new Date();
       // TODO: 対象のライブラリごとに切り替えたい
       const baseDate = new Date(today.setDate(today.getDate() - 1));
