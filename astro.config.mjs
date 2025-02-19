4207c51be5066860fc78301755adb70bfdf765a1
hidev <48004352+newbee1939@users.noreply.github.com>
2025-02-19 14:54:41 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
+import icon from 'astro-icon';
 // @ts-check
 import { defineConfig } from 'astro/config';
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon()],
+  integrations: [icon()],
 });
